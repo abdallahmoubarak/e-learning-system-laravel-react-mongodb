60c2265ea017944e9871411b8737b29a66781a50
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-08 07:04:49 +0300

    initializing react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <Input name="Name" />
+    </div>
+  );
+}
+
+export default App;
