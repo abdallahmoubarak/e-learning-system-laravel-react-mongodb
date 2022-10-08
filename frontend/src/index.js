183a3aee402cef61ee02ae3ad9f67df8ada2f7ec
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-08 23:15:37 +0300

    removing strictin mode frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const client = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
 );
