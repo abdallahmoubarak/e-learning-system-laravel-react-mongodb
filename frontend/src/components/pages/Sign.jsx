cd48626c4e55d7027028585c36b55f81b85187c8
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-08 08:06:53 +0300

    updating sign components frontend

--- frontend/src/components/pages/Sign.jsx
+++ frontend/src/components/pages/Sign.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import Button from "../Button";
 import Input from "../Input";
 
 export default function Sign() {
+  const [signup, setSignUp] = useState(true);
   return (
     <>
       <div className="sign-container">
@@ -10,14 +13,23 @@ export default function Sign() {
           <Input name="Email" />
           <Input name="Password" pass={true} />
         </div>
+        <div className="switch" onClick={() => setSignUp(!signup)}>
+          {signup ? "I already have an account" : "I don't have an account"}
+        </div>
+        <Button text={signup ? "Sign Up" : "Sign In"} />
       </div>
 
       <style jsx>{`
+        h1 {
+          width: 100%;
+          font-size: 2.4rem;
+        }
         .sign-container {
           display: flex;
           flex-direction: column;
           justify-content: center;
-          max-width: 30rem;
+          align-items: center;
+          max-width: 26rem;
           margin: auto;
           padding-top: 3rem;
           color: white;
@@ -27,9 +39,16 @@ export default function Sign() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          padding: 1rem;
           gap: 1rem;
           width: 100%;
           padding: 2rem 0;
+          padding-bottom: 3rem;
+        }
+        .switch {
+          cursor: pointer;
+          text-decoration: underline;
+          padding: 0.2rem;
         }
       `}</style>
     </>
