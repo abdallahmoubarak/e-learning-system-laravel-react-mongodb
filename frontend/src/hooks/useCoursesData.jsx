93e686ae3dc1735cbafc15237eeb1d8838add90a
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-09 20:35:54 +0300

    fixing mutation frontend

--- frontend/src/hooks/useCoursesData.jsx
+++ frontend/src/hooks/useCoursesData.jsx
@@ -1,11 +1,16 @@
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { request } from "../util/axios-utils";
+import { v4 as uuid } from "uuid";
 
 const addCourse = (course) => {
+  const id = uuid();
+  const code = "CS229101";
+  const status = "active";
+  course = { id, code, ...course, status };
   return request({ url: "/courses", data: course, method: "post" });
 };
 
-export const useCoursesData = () => {
+export const useFetchCourses = () => {
   return useQuery("courses", {
     select: (data) => {
       const courses = data?.map((course) => {
--- frontend/src/hooks/useUserData.jsx
+++ frontend/src/hooks/useUserData.jsx
@@ -1,7 +1,11 @@
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { request } from "../util/axios-utils";
+import { v4 as uuid } from "uuid";
 
 const addUser = (user) => {
+  const id = uuid();
+  const code = "122109";
+  user = { id, code, ...user };
   return request({ url: "/users", data: user, method: "post" });
 };
 
