481d78f165fa0f823ffcd34004160579cc44877b
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-10 02:01:05 +0300

    adding assignment and announcement frontend

--- frontend/src/hooks/useCoursesData.jsx
+++ frontend/src/hooks/useCoursesData.jsx
@@ -2,14 +2,6 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { request } from "../util/axios-utils";
 import { v4 as uuid } from "uuid";
 
-const addCourse = (course) => {
-  const id = uuid();
-  const code = "CS229101";
-  const status = "active";
-  course = { id, code, ...course, status };
-  return request({ url: "/courses", data: course, method: "post" });
-};
-
 export const useFetchCourses = () => {
   return useQuery("courses", {
     select: (data) => {
@@ -21,9 +13,55 @@ export const useFetchCourses = () => {
   });
 };
 
+const addCourse = (course) => {
+  const id = uuid();
+  const code = "CS229101";
+  const status = "active";
+  course = { id, code, ...course, status };
+  return request({ url: "/courses", data: course, method: "post" });
+};
+
 export const useAddCourse = () => {
   const queryClient = useQueryClient();
   return useMutation(addCourse, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courses"]);
+    },
+  });
+};
+
+const addAssignment = (assignment) => {
+  const id = uuid();
+  assignment = { id, ...assignment };
+  return request({
+    url: "/courses/assignments",
+    data: assignment,
+    method: "post",
+  });
+};
+
+export const useAddAssignment = () => {
+  const queryClient = useQueryClient();
+  return useMutation(addAssignment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courses"]);
+    },
+  });
+};
+
+const addAnnouncement = (announcement) => {
+  const id = uuid();
+  announcement = { id, ...announcement };
+  return request({
+    url: "/courses/announcements",
+    data: announcement,
+    method: "post",
+  });
+};
+
+export const useAddAnnouncement = () => {
+  const queryClient = useQueryClient();
+  return useMutation(addAnnouncement, {
     onSuccess: () => {
       queryClient.invalidateQueries(["courses"]);
     },
