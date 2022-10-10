f48795c294d666cdc2c4b0368e5a7dd973012731
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-10 23:00:55 +0300

    fixing auth in backend

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -40,12 +40,14 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:8',
+            'type' => 'min:4',
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'type' => $request->type,
             'password' => Hash::make($request->password),
         ]);
 
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -23,6 +23,8 @@ class User extends Authenticatable  implements JWTSubject{
         'name',
         'email',
         'password',
+        'type',
+
     ];
 
     /**
