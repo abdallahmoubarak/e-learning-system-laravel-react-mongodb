29e69d31cbb56a3035f8b03eb58d8e4a9839e773
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 01:54:32 +0300

    setting needed routes backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsersController;
 
 
 Route::group(["prefix"=> "v0.1"], function(){
@@ -13,7 +14,24 @@ Route::group(["prefix"=> "v0.1"], function(){
     });
     
     Route::group(['middleware'=> 'auth:api'], function(){
-     
+
+        Route::controller(UsersController::class)->group(function () {
+            Route::get("/users/instructors", 'getInstructors');
+            Route::get("/users/students", 'getStudents');
+            Route::post("/users", 'addUser');
+            Route::put("/users", 'updateUser');
+        });
+
+        Route::controller(CoursesController::class)->group(function () {
+            Route::get("/courses", 'getCourses');
+            Route::post("/courses", 'createCourse');
+            Route::put("/courses", 'updateCourse');
+            Route::post("/courses/student", 'addStudentToCourse');
+            Route::post("/courses/assignments", 'addAssignment');
+            Route::post("/courses/announcements", 'addAnnouncement');
+
+        });
+
     });
 
 });
