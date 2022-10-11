7df4f5b7c1cf6fa691cb81aec7c81cd81b0a4edd
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 20:46:28 +0300

    updating user model backend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable  implements JWTSubject{
      */
     protected $fillable = [
         'name',
+        'code',
         'email',
         'password',
         'admin_id',
