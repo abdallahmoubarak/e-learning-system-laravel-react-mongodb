974d035c2851ea885a040072afdfddd36665dc7b
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 20:44:29 +0300

    creating course backend

--- backend/app/Http/Controllers/CoursesController.php
+++ backend/app/Http/Controllers/CoursesController.php
@@ -7,21 +7,38 @@ use Illuminate\Http\Request;
 use App\Models\Course;
 
 class CoursesController extends Controller{
-    
-public function getJWTIdentifier(){
-    return $this->getKey();
-}
-
-public function getJWTCustomClaims(){
-    return [];
-}
 
 
 function getCourses(){
-    return Course::where('admin_id', Auth::id());
+   
+    $courses= Course::where('admin_id', Auth::id())->get();
+
+    return response()->json([
+        'status' => 'success',
+        'data' => $courses
+    ]);
+
+    return response()->json(["status" => "Error"]);
+
 }
 
 function createCourse(Request $request){
+
+    $course = Course::create([
+        'admin_id' => Auth::id(),
+        'code' => $request->code,
+        'name' => $request->name,
+        'instructor' => $request->instructor,
+        'instructor_id' => $request->instructor_id,
+    ]);
+    
+    if($course->save()) {
+        return response()->json([
+            "status" => "success",
+            "data" => $course
+        ]);
+    }
+    return response()->json(["status" => "error"]);
 }
 
 function updateCourse(Request $request){
