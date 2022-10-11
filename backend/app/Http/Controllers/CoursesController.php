c3a91f32e1dba2d1e813052e75b4615391c510f2
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 23:08:59 +0300

    adding courses functionality backend

--- backend/app/Http/Controllers/CoursesController.php
+++ backend/app/Http/Controllers/CoursesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Course;
+use App\Models\User;
+
 
 class CoursesController extends Controller{
 
@@ -41,17 +43,67 @@ function createCourse(Request $request){
     return response()->json(["status" => "error"]);
 }
 
-function updateCourse(Request $request){
-}
 
 function addStudentToCourse(Request $request){
+
+    $user = User::where('email', $request->email)
+                ->where('admin_id',$request->admin_id)->first();
+    
+    if($user){
+
+       $course = User::find('id')->first();
+       $course->students[]= $user->id;
+       
+       if($course->save()) {
+        return response()->json([
+            "status" => "success",
+            "data" => $course
+        ]);
+    }
+    }
+    return response()->json(["status" => "error"]);
 }
 
 function addAssignment(Request $request){
+    
+    $course = course::where('id', $request->id)
+                ->where('instructor_id',$request->instructor_id)
+                ->where('admin_id',$request->admin_id)->first();
+    
+    if($course){
+       $course->assignments[]= ['title'=>$request->title,
+                                'content'=>$request->content,
+                                'due'=>$request->due
+                                ];
+       
+       if($course->save()) {
+        return response()->json([
+            "status" => "success",
+            "data" => $course
+        ]);
+    }
+    }
+    return response()->json(["status" => "error"]);
 }
 
 function addAnnouncement(Request $request){
+
+    $course = course::where('id', $request->id)
+                ->where('instructor_id',$request->instructor_id)
+                ->where('admin_id',$request->admin_id)->first();
+
+    if($course){
+        $course->announcements[]= ['title'=>$request->title,
+                                    'content'=>$request->content
+                                    ];
+
+        if($course->save()) {
+            return response()->json([
+            "status" => "success",
+            "data" => $course
+            ]);
+        }
+    return response()->json(["status" => "error"]);
+    }
 }
-
-
 }
