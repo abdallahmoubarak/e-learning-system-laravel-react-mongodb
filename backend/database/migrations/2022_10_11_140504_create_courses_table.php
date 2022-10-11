c4b6a66f97e8d8960613b2d7ecf056717fa5c0ac
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 20:47:07 +0300

    updating migrations backend

---
+++ backend/database/migrations/2022_10_11_140504_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string(code);
+            $table->string(name);
+            $table->string(admin_id);
+            $table->string(instructor_id);
+            $table->string(instructor);
+            $table->array(assignments);
+            $table->array(students);
+            $table->array(announcements);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
