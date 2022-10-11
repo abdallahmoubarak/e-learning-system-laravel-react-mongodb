c2ba1c6013273a8e1137cafd01fa0ae47f47441d
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 20:47:37 +0300

    using ReactQueryDevtools frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const client = new QueryClient({});
+export const client = new QueryClient({});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={client}>
+    <ReactQueryDevtools />
     <App />
   </QueryClientProvider>,
 );
