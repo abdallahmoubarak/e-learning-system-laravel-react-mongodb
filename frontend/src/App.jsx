55d9616bb429ad1d814a8de479c764051be2db9b
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-11 20:49:51 +0300

    changing auth state strategy frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,22 @@
 import "./app.css";
-import { useState } from "react";
 import Admin from "./components/pages/Admin";
 import Instructor from "./components/pages/Instructor";
 import SignPage from "./components/pages/SignPage";
 import Student from "./components/pages/Student";
-import { useEffect } from "react";
+import { useCurrentUser } from "./hooks/useSign";
 
 function App() {
-  const [auth, setAuth] = useState(true);
-  const [currentUser, setCurrentUser] = useState("");
-
-  useEffect(() => {
-    setAuth(!!localStorage.getItem("JWT"));
-    !!localStorage.getItem("User") &&
-      setCurrentUser(JSON.parse(localStorage.getItem("User")));
-  }, [auth]);
+  const { data: currentUser, isLoading } = useCurrentUser();
 
   return (
     <>
-      <SignPage auth={auth} setAuth={setAuth} />
-      {auth && (
-        <div className={`app-body`}>
+      {isLoading ? (
+        <div className="fill-back"></div>
+      ) : (
+        <SignPage currentUser={currentUser} />
+      )}
+      {Boolean(currentUser) && (
+        <div className="app-body">
           {currentUser.type === "Admin" && <Admin />}
           {currentUser.type === "Instructor" && <Instructor />}
           {currentUser.type === "Student" && <Student />}
